f8c2f28ae396553d298a99b333bd4a2a882e6550
Nadine Olivia <nadine16olivia@gmail.com>
2026-04-27 12:22:41 +0800

    feat: my learn page

---
+++ app/learn/page.tsx
@@ -0,0 +1,38 @@
+export default function learnpage() {
+    return (
+        <section className="h-screen bg-gray-100 flex gap-4">
+        <div className="bg-purple-300 p-6 h-72 w-96 rounded-4xl flex flex-col justify-between text-blue-950">
+            <div className="flex gap-2">
+                <img src="palette.svg" alt="Design"/>
+                <p className="font-bold">Design</p>
+            </div>
+            <div>
+                <h2 className="text-4xl font-bold">Figma</h2>
+                <p className="font-bold">8 Hours ago</p>
+            </div>
+        </div>
+
+         <div className="bg-orange-300 p-6 h-72 w-96 rounded-4xl flex flex-col justify-between text-blue-950">
+            <div className="flex gap-2">
+                <img src="code.svg" alt="Design"/>
+                <p className="font-bold">Coding</p>
+            </div>
+            <div>
+                <h2 className="text-4xl font-bold">Python</h2>
+                <p className="font-bold">2 days ago</p>
+            </div>
+        </div>
+
+         <div className="bg-red-300 p-6 h-72 w-96 rounded-4xl flex flex-col justify-between text-blue-950">
+            <div className="flex gap-2">
+                <img src="palette.svg" alt="Design"/>
+                <p className="font-bold">Design</p>
+            </div>
+            <div>
+                <h2 className="text-4xl font-bold">Sketch</h2>
+                <p className="font-bold">4 days ago</p>
+            </div>
+        </div>
+        </section>
+    );
+}
